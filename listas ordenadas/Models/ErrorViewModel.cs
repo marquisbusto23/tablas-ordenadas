3604f47cd6189f9789695f671790f4d47132f26d
marcos busto <marcosbustos694@gmail.com>
2019-02-26 10:31:47 -0300

    Agregar archivos de proyecto.

---
+++ listas ordenadas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace listas_ordenadas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
